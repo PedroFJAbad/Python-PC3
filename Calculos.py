dd12ba154860e773f2b742521fe34f80e93059f1
Pedro Francisco Jacinto Abad <162522759+PedroFJAbad@users.noreply.github.com>
2024-03-30 02:05:53 +0000

    Calculos

---
+++ Calculos.py
@@ -0,0 +1,10 @@
+import Problema9
+
+# Ejemplo de uso
+a = 20
+b = 4
+
+print(f"Suma: {Problema9.suma(a, b)}")
+print(f"Resta: {Problema9.resta(a, b)}")
+print(f"Producto: {Problema9.producto(a, b)}")
+print(f"División: {Problema9.division(a, b)}")
